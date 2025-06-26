0d3201145a55ff325acc1dff724e0340d80a3ea9
Victor Escalera Solis <victor.solis2019@gmail.com>
2025-06-25 19:51:13 -0600

    second commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Cambia "formularioDB" por el nombre que quieras para tu base de datos
+const formularioDB = "mongodb+srv://victorsolis2019:Tobivictor__11@cluster0.syaedri.mongodb.net/formularioDB?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(formularioDB , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("✅ Conectado a MongoDB Atlas"))
+  .catch(err => console.error("❌ Error de conexión", err));
+
+const UsuarioSchema = new mongoose.Schema({
+  nombre: String,
+  email: String,
+  carrera: String,
+  telefono: String,
+  fecha: Date,
+});
+
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
+
+app.post("/enviar", async (req, res) => {
+  try {
+    console.log("📩 Datos recibidos:", req.body);
+    const nuevoUsuario = new Usuario(req.body);
+    await nuevoUsuario.save();
+    res.json({ mensaje: "✅ Datos guardados en MongoDB Atlas" });
+  } catch (error) {
+    console.error("❌ Error:", error);
+    res.status(500).json({ mensaje: "❌ Error al guardar datos", error });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Servidor corriendo en http://localhost:3000");
+});
---
+++ index.js
@@ -0,0 +1,25 @@
+document.getElementById("miFormulario").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const datos = {
+    nombre: document.getElementById("nombre").value,
+    email: document.getElementById("email").value,
+    carrera: document.getElementById("carrera").value,
+    telefono: document.getElementById("telefono").value,
+    fecha: document.getElementById("fecha").value,
+  };
+
+  try {
+    const response = await fetch('http://localhost:3000/enviar', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(datos),
+    });
+
+    const result = await response.json();
+    alert(result.mensaje || 'Formulario enviado correctamente');
+    document.getElementById("miFormulario").reset();
+  } catch (error) {
+    alert("Error al enviar datos: " + error.message);
+  }
+});
